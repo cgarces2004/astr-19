1c44cd5ce9466b7fc9d4ce7e06c9bcd48f2950e6
cgarces2004 <156133515+cgarces2004@users.noreply.github.com>
2024-01-12 12:37:17 -0800

    Code Journal Prompt 2

---
+++ code_journal2.py
@@ -0,0 +1,29 @@
+#Print the sum of two floating point numbers
+num = 420.20
+num2 = 79.34
+sum_num = num + num2 
+print(f'The sum of {num} and {num2} is {sum_num}')
+#Print out the data type of the resulting answer
+print(type(sum_num))
+print('\n')
+
+
+#Print the difference of two integer numbers
+int1 = 5
+int2 = 10
+int_diff = (int1 - int2)
+print(f'The difference of {int1} and {int2} is {int_diff}')
+#Print out the data type of the resulting number
+print(type(int_diff))
+print('\n')
+
+
+
+#Print the product of a floating point number and an integer
+float1 = 32.45
+int3 = 10
+product= (float1 * int3)
+print(f'The product of {float1} and {int3} is {product}')
+#Print out the datat type of the resulting number
+print(type(product))
+print('\n')
