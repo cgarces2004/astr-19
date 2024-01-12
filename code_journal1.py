346050ceb35fd9f18d7b5dcf7a14056217b68481
cgarces2004 <156133515+cgarces2004@users.noreply.github.com>
2024-01-12 12:20:46 -0800

    Prompt 1 Coding Journal 1

---
+++ code_journal1.py
@@ -0,0 +1,7 @@
+name = input('Hello, what is your name?')
+pronouns = input('What are your pronouns?')
+food = input('What is your favorite food?')
+movie = input('What is your favorite movie?')
+
+print(f'My name is {name}, Pronouns: {pronouns}, Favorite Food: {food}, Favorite Movie: {movie}')
+
