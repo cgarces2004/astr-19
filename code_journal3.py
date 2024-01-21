c6be11c98f6d986cd87566c86cbf22d462cbd1e9
cgarces2004 <156133515+cgarces2004@users.noreply.github.com>
2024-01-21 02:34:12 -0800

    Add files via upload

---
+++ code_journal3.py
@@ -0,0 +1,15 @@
+# Define Function F with argument x
+# Arithmetic Instructions for Function F
+def f(x):
+	return x**3 + 8
+
+# Define Main
+def main():
+	#set x = 9
+	result = f(9)
+	# If else statement for printing YAY
+	if result > 27:
+		print('YAY!')
+
+if __name__ == '__main__':
+	main()
